545a8023b0241a3e9c5d8fd23ff78857bb32eacd
Lukas Holmberg <lukazlovicc@hotmail.com>
2019-03-13 17:17:07 +0100

    Updated

--- SlackClone/Controller/ChannelVC.swift
+++ SlackClone/Controller/ChannelVC.swift
@@ -10,16 +10,6 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
-    
-    
-    // OUTLETS
-    
-    @IBOutlet weak var loginBtn: UIButton!
-    
-    @IBAction func prepareforUnwind(segue: UIStoryboardSegue){
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
@@ -29,8 +19,5 @@ class ChannelVC: UIViewController {
     
 
     
-    @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
-    }
-    
+
 }
